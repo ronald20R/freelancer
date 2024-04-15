6e56028fd913a1d0e207868b27c5adc47c08d8f1
ronald20R <ronaldavidcondoris@gmail.com>
2024-04-15 18:42:31 -0500

    agregando el avance de la maquetacion

---
+++ app.js
@@ -0,0 +1,11 @@
+console.log("hola mundo");
+
+
+let diasemana = ["lunes", "martes","miercoles","jueves"];
+
+console.log(diasemana[3]);
+// 2
+
+let ultimo =diasemana.pop(3);
+
+console.log(diasemana)
